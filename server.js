75c177606b1706a3c8777a3584e7e1fe3f04f33f
coelhoptbr <coelhoptbr@gmail.com>
2019-08-19 00:23:47 -0300

    pub heroku

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
+const path = require('path');
 const express = require('express');
+const app = express();
 
-const path = require('path');   
+// Serve static files
+app.use(express.static(__dirname + '/dist/clientesite'));
 
-const app = express();   
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/clientesite/index.html'));
+});
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
-});  
-
-// Start the app by listening on the default Heroku port    
+// default Heroku port
 app.listen(process.env.PORT || 8080);
